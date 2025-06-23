55642fd143cbe68d437757f3a80895797ad3cf7d
Paulo Sebastián García Vargas <psebastian21@gmail.com>
2025-06-23 18:47:38 -0500

    Two way data binding

---
+++ src/components/Player.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react"
+
+const Player = ({ initialName, symbol }) => {
+    const [isEditing, setIsEditing] = useState(false)
+    const [playerName, setPlayerName] = useState(initialName)
+    const handleEditClick = () => {
+        setIsEditing((prevIsEditing) => !prevIsEditing)
+    }
+    const handleInputChange = (event) => {
+        setPlayerName(event.target.value)
+    }
+    let playerNameField, buttonText
+    if(isEditing){
+         playerNameField = <input type="text" required value={playerName} onChange={handleInputChange}/> 
+         buttonText="Save"
+    }
+    else{
+        playerNameField = <span className="player-name">{playerName}</span>
+        buttonText = "Edit"
+    }
+    return <li>
+        <span className="player">
+            {playerNameField}
+            <span className="player-symbol">{symbol}</span>
+        </span>
+        <button onClick={handleEditClick}>{buttonText}</button>
+    </li>
+}
+
+export default Player
